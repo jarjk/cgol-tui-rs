7e0029470f209d79c0ee44256627c466557ef4a4
Jeromos Kovács <iitsnotme214@proton.me>
2026-06-24 16:39:47 +0200

    WIP: misc: crossterm -> termina backend

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,64 @@
-use app::{App, Universe};
+use std::io::Write as _;
 use std::{io::Read, str::FromStr};
 
+use app::{App, Universe};
+use ratatui::backend::TerminaBackend;
+use ratatui::termina::escape::csi::{self, Csi};
+use ratatui::termina::{EventReader, PlatformTerminal, Terminal as _};
+use ratatui::Terminal;
+
 pub mod app;
+
+type AppTerminal = Terminal<TerminaBackend<PlatformTerminal>>;
+
+macro_rules! decset {
+    ($mode:ident) => {{
+        let mode = csi::DecPrivateMode::Code(csi::DecPrivateModeCode::$mode);
+        Csi::Mode(csi::Mode::SetDecPrivateMode(mode))
+    }};
+}
+
+macro_rules! decreset {
+    ($mode:ident) => {{
+        let mode = csi::DecPrivateMode::Code(csi::DecPrivateModeCode::$mode);
+        Csi::Mode(csi::Mode::ResetDecPrivateMode(mode))
+    }};
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let arg_universes = parse_args()?;
 
+    let (mut terminal, events) = init_terminal()?;
     let mut app = App::default().with_universes(arg_universes);
+    let res = app.run(&mut terminal, &events);
 
-    let mut terminal = ratatui::try_init()?;
-
-    let res = app.run(&mut terminal);
-
-    ratatui::try_restore()?;
+    restore_terminal(&mut terminal)?;
 
     Ok(res?)
+}
+
+fn init_terminal() -> Result<(AppTerminal, EventReader), Box<dyn std::error::Error>> {
+    let mut output = PlatformTerminal::new()?;
+    output.enter_raw_mode()?;
+
+    let enter_alternate_screen = decset!(ClearAndEnableAlternateScreen);
+    let show_cursor = decset!(ShowCursor);
+    write!(output, "{enter_alternate_screen}{show_cursor}")?;
+    output.flush()?;
+
+    let events = output.event_reader();
+    let backend = TerminaBackend::new(output);
+    let terminal = Terminal::new(backend)?;
+    Ok((terminal, events))
+}
+
+fn restore_terminal(terminal: &mut AppTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    let leave_alternate_screen = decreset!(ClearAndEnableAlternateScreen);
+    let show_cursor = decset!(ShowCursor);
+    let backend = terminal.backend_mut();
+    write!(backend, "{leave_alternate_screen}{show_cursor}")?;
+    backend.flush()?;
+    Ok(())
 }
 
 fn parse_args() -> Result<Vec<Universe>, Box<dyn std::error::Error>> {
